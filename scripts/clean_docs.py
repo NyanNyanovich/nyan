5c718a5e3db274a46afef21fbb23fe3eaee277c0
Nyan Nyanovich <nyan_news@protonmail.com>
2022-05-21 22:08:54 +0000

    WhileTrue send.sh and minor scripts fixes

--- scripts/clean_docs.py
+++ scripts/clean_docs.py
@@ -1,15 +1,41 @@
+import argparse
 import json
-import sys
+
 from nyan.annotator import Annotator
 from nyan.document import Document
 
-input_path = sys.argv[1]
-output_path = sys.argv[2]
 
-annotator = Annotator("configs/annotator_config.json", "channels.json")
+def main(
+    input_path,
+    output_path,
+    annotator_config,
+    channels_path,
+    rm_fields,
+    min_pub_time
+):
+    rm_fields = rm_fields.split(",")
+    annotator = Annotator(annotator_config, channels_path)
 
-with open(input_path) as r, open(output_path, "w") as w:
-    docs = [Document.fromdict(json.loads(line)) for line in r]
-    clean_docs = annotator(docs)
-    for doc in clean_docs:
-        w.write(json.dumps(doc.asdict(save_embedding=True), ensure_ascii=False).strip() + "\n")
+    with open(input_path) as r, open(output_path, "w") as w:
+        docs = [Document.fromdict(json.loads(line)) for line in r]
+        clean_docs = annotator(docs)
+        clean_docs = [doc for doc in clean_docs if doc.pub_time >= min_pub_time]
+        clean_docs.sort(key=lambda x: x.pub_time)
+
+        for doc in clean_docs:
+            doc = doc.asdict()
+            for field in rm_fields:
+                doc.pop(field)
+            w.write(json.dumps(doc, ensure_ascii=False).strip() + "\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output-path", type=str, required=True)
+    parser.add_argument("--input-path", type=str, required=True)
+    parser.add_argument("--annotator-config", type=str, default="configs/annotator_config.json")
+    parser.add_argument("--channels-path", type=str, default="channels.json")
+    parser.add_argument("--rm-fields", type=str, default="embedding,category")
+    parser.add_argument("--min-pub-time", type=int, default=1649138400)
+    args = parser.parse_args()
+    main(**vars(args))
--- scripts/mongo_to_jsonl.py
+++ scripts/mongo_to_jsonl.py
@@ -1,24 +1,34 @@
-import sys
+import argparse
 import json
 
 from pymongo import MongoClient
 
-output_path = sys.argv[1]
-assert output_path, "Provide output path!"
 
-with open("configs/prod_mongo_config.json") as r:
-    config = json.load(r)
+def main(
+    output_path,
+    mongo_config
+):
+    with open(mongo_config) as r:
+        config = json.load(r)
 
-client = MongoClient(**config["client"])
-database_name = config["database_name"]
-documents_collection_name = config["documents_collection_name"]
-collection = client[database_name][documents_collection_name]
+    client = MongoClient(**config["client"])
+    database_name = config["database_name"]
+    documents_collection_name = config["documents_collection_name"]
+    collection = client[database_name][documents_collection_name]
 
-print(collection.count_documents({}))
+    print(collection.count_documents({}))
 
-with open(output_path, "w") as w:
-    documents = list(collection.find({}))
-    documents.sort(key=lambda x: x["pub_time"])
-    for document in documents:
-        document.pop("_id")
-        w.write(json.dumps(document, ensure_ascii=False) + "\n")
+    with open(output_path, "w") as w:
+        documents = list(collection.find({}))
+        documents.sort(key=lambda x: x["pub_time"])
+        for document in documents:
+            document.pop("_id")
+            w.write(json.dumps(document, ensure_ascii=False) + "\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output-path", type=str, default="data/docs.jsonl")
+    parser.add_argument("--mongo-config", type=str, default="configs/mongo_config.json")
+    args = parser.parse_args()
+    main(**vars(args))
