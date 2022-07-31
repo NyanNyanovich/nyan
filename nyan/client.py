8ae9fcb59328f82c536a386a6bd57d3db3fe316e
Nyan Nyanovich <nyan_news@protonmail.com>
2022-07-31 21:17:38 +0000

    Tech issue

--- nyan/client.py
+++ nyan/client.py
@@ -1,9 +1,18 @@
 import os
 import json
 from time import sleep
+from dataclasses import dataclass
 
-import requests
 from httpx import Timeout, Limits, HTTPTransport, Client
+
+
+@dataclass
+class IssueConfig:
+    name: str
+    channel_id: int
+    discussion_id: int
+    bot_token: str
+    last_update_id: int = 0
 
 
 class TelegramClient:
@@ -31,23 +40,22 @@ class TelegramClient:
             transport=transport
         )
 
-        self.channel_id = self.config["channel_id"]
-        self.discussion_id = self.config["discussion_id"]
-        self.bot_token = self.config["bot_token"]
-        self.discussions = dict()
-        self.last_update_id = 0
-        self.update_discussion_mapping()
+        self.issues = {config["name"]: IssueConfig(**config) for config in self.config["issues"]}
+        self.discussions = {issue.name: dict() for _, issue in self.issues.items()}
+        for issue_name in self.issues:
+            self.update_discussion_mapping(issue_name)
 
-    def send_message(self, text, photos=tuple(), videos=tuple()):
+    def send_message(self, text, issue_name, photos=tuple(), videos=tuple()):
+        issue = self.issues[issue_name]
         response = None
         if len(photos) == 1:
-            response = self.send_photo(text, photos[0])
+            response = self._send_photo(text, photos[0], issue=issue)
         elif len(photos) > 1:
-            response = self.send_photos(text, photos)
+            response = self._send_photos(text, photos, issue=issue)
         elif len(videos) >= 1:
-            response = self.send_video(text, videos[0])
+            response = self._send_video(text, videos[0], issue=issue)
         else:
-            response = self.send_text(text)
+            response = self._send_text(text, issue=issue)
 
         print("Send status code:", response.status_code)
         if response.status_code != 200:
@@ -58,116 +66,19 @@ class TelegramClient:
         message_id = int(result["message_id"] if "message_id" in result else result[0]["message_id"])
         return message_id
 
-    def update_message(self, message_id, text, is_caption):
-        response = None
+    def update_message(self, message_id, text, is_caption, issue_name):
+        issue = self.issues[issue_name]
         if not is_caption:
-            response = self.edit_text(message_id, text)
+            response = self._edit_text(message_id, text, issue=issue)
         else:
-            response = self.edit_caption(message_id, text)
-
+            response = self._edit_caption(message_id, text, issue=issue)
         print("Update status code:", response.status_code)
         if response.status_code != 200:
             print("Update error:", response.text)
 
-    def send_text(self, text):
-        url_template = "https://api.telegram.org/bot{}/sendMessage"
-        params = {
-            "chat_id": self.channel_id,
-            "text": text,
-            "parse_mode": "html",
-            "disable_web_page_preview": True,
-            "disable_notification": True
-        }
-        return self.post(url_template.format(self.bot_token), params)
-
-    def send_photo(self, text, photo):
-        url_template = "https://api.telegram.org/bot{}/sendPhoto"
-        params = {
-            "chat_id": self.channel_id,
-            "caption": text,
-            "photo": photo,
-            "parse_mode": "html",
-            "disable_notification": True
-        }
-        return self.post(url_template.format(self.bot_token), params)
-
-    def send_video(self, text, video):
-        url_template = "https://api.telegram.org/bot{}/sendVideo"
-        params = {
-            "chat_id": self.channel_id,
-            "caption": text,
-            "video": video,
-            "parse_mode": "html",
-            "disable_notification": True
-        }
-        return self.post(url_template.format(self.bot_token), params)
-
-    def send_photos(self, text, photos):
-        url_template = "https://api.telegram.org/bot{}/sendMediaGroup"
-        media = [{
-            "type": "photo",
-            "media": photo,
-            "caption": text if i == 0 else "",
-            "parse_mode": "html"
-        } for i, photo in enumerate(photos)]
-        params = {
-            "chat_id": self.channel_id,
-            "disable_notification": True,
-            "media": json.dumps(media)
-        }
-        return self.post(url_template.format(self.bot_token), params)
-
-    def edit_text(self, message_id, text):
-        url_template = "https://api.telegram.org/bot{}/editMessageText"
-        params = {
-            "chat_id": self.channel_id,
-            "text": text,
-            "parse_mode": "html",
-            "disable_web_page_preview": True,
-            "message_id": message_id
-        }
-        return self.post(url_template.format(self.bot_token), params)
-
-    def edit_caption(self, message_id, text):
-        url_template = "https://api.telegram.org/bot{}/editMessageCaption"
-        params = {
-            "chat_id": self.channel_id,
-            "message_id": message_id,
-            "caption": text,
-            "parse_mode": "html",
-        }
-        return self.post(url_template.format(self.bot_token), params)
-
-    def send_discussion_message(self, text, reply_to_message_id):
-        if not self.discussion_id or not reply_to_message_id:
-            return None
-        url_template = "https://api.telegram.org/bot{}/sendMessage"
-        params = {
-            "chat_id": self.discussion_id,
-            "text": text,
-            "parse_mode": "html",
-            "disable_web_page_preview": False,
-            "reply_to_message_id": reply_to_message_id
-        }
-        return self.post(url_template.format(self.bot_token), params)
-
-    def get_updates(self):
-        url_template = "https://api.telegram.org/bot{}/getUpdates"
-        params = {
-            "timeout": 10
-        }
-        if self.last_update_id != 0:
-            params["offset"] = self.last_update_id
-        response = self.client.get(url_template.format(self.bot_token), params=params, timeout=20)
-        if response.status_code != 200:
-            return None
-        updates = response.json()["result"]
-        for update in updates:
-            self.last_update_id = max(self.last_update_id, update["update_id"]) + 1
-        return updates
-
-    def update_discussion_mapping(self):
-        updates = self.get_updates()
+    def update_discussion_mapping(self, issue_name):
+        issue = self.issues[issue_name]
+        updates = self._get_updates(issue)
         if not updates:
             return dict()
         for update in updates:
@@ -176,16 +87,114 @@ class TelegramClient:
             message = update["message"]
             if "forward_from_chat" not in message:
                 continue
-            if self.channel_id != message["forward_from_chat"]["id"]:
+            if issue.channel_id != message["forward_from_chat"]["id"]:
                 continue
-            if self.discussion_id != message["chat"]["id"]:
+            if issue.discussion_id != message["chat"]["id"]:
                 continue
             orig_message_id = message["forward_from_message_id"]
             discussion_message_id = message["message_id"]
-            self.discussions[orig_message_id] = discussion_message_id
+            self.discussions[issue.name][orig_message_id] = discussion_message_id
 
-    def get_discussion(self, message_id):
-        return self.discussions.get(message_id, None)
+    def get_discussion(self, message_id, issue_name):
+        return self.discussions[issue_name].get(message_id, None)
 
-    def post(self, url, params):
+    def send_discussion_message(self, text, reply_to_message_id, issue_name):
+        issue = self.issues[issue_name]
+        if not issue.discussion_id or not reply_to_message_id:
+            return None
+        url_template = "https://api.telegram.org/bot{}/sendMessage"
+        params = {
+            "chat_id": issue.discussion_id,
+            "text": text,
+            "parse_mode": "html",
+            "disable_web_page_preview": False,
+            "reply_to_message_id": reply_to_message_id
+        }
+        return self._post(url_template.format(issue.bot_token), params)
+
+    def _send_text(self, text, issue):
+        url_template = "https://api.telegram.org/bot{}/sendMessage"
+        params = {
+            "chat_id": issue.channel_id,
+            "text": text,
+            "parse_mode": "html",
+            "disable_web_page_preview": True,
+            "disable_notification": True
+        }
+        return self._post(url_template.format(issue.bot_token), params)
+
+    def _send_photo(self, text, photo, issue):
+        url_template = "https://api.telegram.org/bot{}/sendPhoto"
+        params = {
+            "chat_id": issue.channel_id,
+            "caption": text,
+            "photo": photo,
+            "parse_mode": "html",
+            "disable_notification": True
+        }
+        return self._post(url_template.format(issue.bot_token), params)
+
+    def _send_video(self, text, video, issue):
+        url_template = "https://api.telegram.org/bot{}/sendVideo"
+        params = {
+            "chat_id": issue.channel_id,
+            "caption": text,
+            "video": video,
+            "parse_mode": "html",
+            "disable_notification": True
+        }
+        return self._post(url_template.format(issue.bot_token), params)
+
+    def _send_photos(self, text, photos, issue):
+        url_template = "https://api.telegram.org/bot{}/sendMediaGroup"
+        media = [{
+            "type": "photo",
+            "media": photo,
+            "caption": text if i == 0 else "",
+            "parse_mode": "html"
+        } for i, photo in enumerate(photos)]
+        params = {
+            "chat_id": issue.channel_id,
+            "disable_notification": True,
+            "media": json.dumps(media)
+        }
+        return self._post(url_template.format(issue.bot_token), params)
+
+    def _edit_text(self, message_id, text, issue):
+        url_template = "https://api.telegram.org/bot{}/editMessageText"
+        params = {
+            "chat_id": issue.channel_id,
+            "text": text,
+            "parse_mode": "html",
+            "disable_web_page_preview": True,
+            "message_id": message_id
+        }
+        return self._post(url_template.format(issue.bot_token), params)
+
+    def _edit_caption(self, message_id, text, issue):
+        url_template = "https://api.telegram.org/bot{}/editMessageCaption"
+        params = {
+            "chat_id": issue.channel_id,
+            "message_id": message_id,
+            "caption": text,
+            "parse_mode": "html",
+        }
+        return self._post(url_template.format(issue.bot_token), params)
+
+    def _get_updates(self, issue):
+        url_template = "https://api.telegram.org/bot{}/getUpdates"
+        params = {
+            "timeout": 10
+        }
+        if issue.last_update_id != 0:
+            params["offset"] = issue.last_update_id
+        response = self.client.get(url_template.format(issue.bot_token), params=params, timeout=20)
+        if response.status_code != 200:
+            return None
+        updates = response.json()["result"]
+        for update in updates:
+            issue.last_update_id = max(issue.last_update_id, update["update_id"]) + 1
+        return updates
+
+    def _post(self, url, params):
         return self.client.post(url, data=params)
