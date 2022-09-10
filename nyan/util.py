925888529877bcb15b727820c2de01e0bdb93431
Nyan Nyanovich <nyan_news@protonmail.com>
2022-09-10 10:37:10 +0000

    torch.use_deterministic_algorithms

--- nyan/util.py
+++ nyan/util.py
@@ -67,3 +67,4 @@ def set_random_seed(seed):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
+    torch.use_deterministic_algorithms(True)
