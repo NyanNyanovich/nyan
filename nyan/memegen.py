79534e93efc5976de45792ddacc8dd756cb9093f
NyanNyanovich <nyan_news@protonmail.com>
2023-08-24 23:54:24 +0000

    imgflip -> memegen

--- nyan/memegen.py
+++ nyan/memegen.py
@@ -13,17 +13,26 @@ from nyan.util import get_current_ts, ts_to_dt, gen_batch
 from nyan.openai import openai_completion
 
 
-def create_meme_request(
+ALL_MEME_TEMPLATES = requests.get("https://api.memegen.link/templates").json()
+
+
+def get_memegen_meme(
     clusters,
     issue_name,
     prompt_path,
     duration_hours,
-    model_name
+    model_name,
+    templates_count
 ):
     with open(prompt_path) as f:
         template = Template(f.read())
 
-    prompt = template.render(clusters=clusters).strip() + "\n"
+    meme_templates = random.sample(ALL_MEME_TEMPLATES, templates_count)
+
+    prompt = template.render(
+        clusters=clusters,
+        meme_templates=meme_templates
+    ).strip() + "\n"
     print(prompt)
 
     messages = [{"role": "user", "content": prompt}]
@@ -32,49 +41,27 @@ def create_meme_request(
     print(content)
 
     content = content[content.find("{"):content.rfind("}") + 1]
-    return json.loads(content)
-
-
-def get_imgflip_meme(
-    request,
-    imgflip_config_path
-):
-    with open(imgflip_config_path) as r:
-        config = json.load(r)
-    request["username"] = config["username"]
-    request["password"] = config["password"]
-
-    if "boxes" in request:
-        for i, box in enumerate(request.pop("boxes")):
-            request[f"boxes[{i}][text]"] = box["text"]
-
-    url = "https://api.imgflip.com/caption_image"
-    response = requests.post(url, data=request)
-
-    assert response.status_code == 200
-    response = response.json()
-    assert response["success"]
-    return response["data"]["url"]
+    response = json.loads(content)
+    url = response["url"] + "?font=impact"
+    explanation = response["explanation"]
+    return url, explanation
 
 
 def main(
     mongo_config_path,
     client_config_path,
-    imgflip_config_path,
     duration_hours,
     max_news_count,
-    min_news_count,
     issue_name,
     prompt_path,
     template_path,
     model_name,
-    auto
+    auto,
+    templates_count
 ):
     duration = int(duration_hours * 3600)
     clusters = Clusters.load_from_mongo(mongo_config_path, get_current_ts(), duration)
     clusters = list(clusters.clid2cluster.values())
-    if len(clusters) < min_news_count:
-        return
     clusters.sort(key=lambda cl: cl.create_time)
     fixed_clusters = []
     for cluster in clusters:
@@ -85,18 +72,18 @@ def main(
     clusters = fixed_clusters
     clusters = clusters[-max_news_count:]
 
-    request = create_meme_request(
+    image_url, explanation = get_memegen_meme(
         clusters,
         issue_name=issue_name,
         prompt_path=prompt_path,
         duration_hours=duration_hours,
-        model_name=model_name
+        model_name=model_name,
+        templates_count=templates_count
     )
-    image_url = get_imgflip_meme(request, imgflip_config_path)
     print("URL:", image_url)
     with open(template_path, "r") as f:
         template = Template(f.read())
-    text = template.render(image_url=image_url)
+    text = template.render(explanation=explanation)
 
     should_publish = False
     if not auto:
@@ -111,10 +98,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--mongo-config-path", type=str, required=True)
     parser.add_argument("--client-config-path", type=str, required=True)
-    parser.add_argument("--imgflip-config-path", type=str, required=True)
     parser.add_argument("--duration-hours", type=int, default=12)
     parser.add_argument("--max-news-count", type=int, default=10)
-    parser.add_argument("--min-news-count", type=int, default=3)
+    parser.add_argument("--templates-count", type=int, default=6)
     parser.add_argument("--issue-name", type=str, default="main")
     parser.add_argument("--prompt-path", type=str, required=True)
     parser.add_argument("--template-path", type=str, required=True)
