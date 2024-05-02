a9d6b9129bebd218c1f0f82057bfe7a9f11249d0
TriplEight <pisarevden@gmail.com>
2024-05-02 22:04:30 +0200

    execute everything in docker-compose

--- send_container.sh
+++ send_container.sh
@@ -7,7 +7,7 @@ do
     python3 -m nyan.send \
         --channels-info-path channels.json \
         --client-config-path configs/client_config.json \
-        --mongo-config-path configs/mongo_config.json \
+        --mongo-config-path configs/mongo_container_config.json \
         --annotator-config-path configs/annotator_config.json \
         --renderer-config-path configs/renderer_config.json \
         --daemon-config-path configs/daemon_config.json;
